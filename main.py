b1225a156ff1160486ace85711063e602f37dc9b
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-03-10 11:29:49 +0500

    turtle module added

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timy_the_turtle = Turtle()
+timy_the_turtle.shape("turtle")
+timy_the_turtle.color("red")
+timy_the_turtle.forward(100)
+timy_the_turtle.right(90)
+timy_the_turtle.forward(20)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
