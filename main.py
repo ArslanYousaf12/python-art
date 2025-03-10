cf612170dfa3aa0dd75818fa50ed8df191cd3b58
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-03-10 11:52:39 +0500

    installing modules with pip3

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+# import turtle
+# import turtle as t
+# from turtle import *
+# import heroes
+# print(heroes.gen())
+
 from turtle import Turtle, Screen
+
+def move_right():
+    timy_the_turtle.right(90)
+    timy_the_turtle.forward(100)
 
 timy_the_turtle = Turtle()
 timy_the_turtle.shape("turtle")
 timy_the_turtle.color("red")
 timy_the_turtle.forward(100)
-timy_the_turtle.right(90)
-timy_the_turtle.forward(20)
+for _ in range(3):
+    move_right()
+    
 
 
 
