e23e7d5638b7e04523635a7d2f40c785e54b1bcc
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-03-11 13:15:01 +0500

    diff shapes draw with turtle

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # print(heroes.gen())
 
 from turtle import Turtle, Screen
+import random
 
 # def move_right():
 #     timy_the_turtle.right(90)
@@ -16,23 +17,37 @@ from turtle import Turtle, Screen
 # timy_the_turtle.forward(100)
 # for _ in range(3):
 #     move_right()
-def movePenDown():
-    timy.pendown()
-    timy.forward(10)
+# def movePenDown():
+#     timy.pendown()
+#     timy.forward(10)
 
-def movePenUp():
-    timy.penup()
-    timy.forward(10)
+# def movePenUp():
+#     timy.penup()
+#     timy.forward(10)
+lineColor = ["plum", "olive", "salmon", "orange"]
 
+
+def moveRight(angel):
+    timy.forward(100)
+    timy.right(angel)
+    
+def drawShape(sides):
+    for _ in range(sides):
+        penColor = random.choice(lineColor)
+        timy.pencolor(penColor)
+        moveRight(total_angel/sides)
 
 
 timy = Turtle()
 timy.shape("turtle")
-for _ in range(5):
-    movePenDown()
-    movePenUp()
+total_angel = 360
+for num in range(3, 11):
+    print(num)
+    drawShape(num)
 
-    
+
+
+        
 
 
 
