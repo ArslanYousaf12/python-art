d6743b971a01ceb21610e8b17beaab8706a976c0
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-03-12 11:07:56 +0500

    color extracted

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import colorgram
+# import colorgram
 
-colors = colorgram.extract('image.jpg',10)
-color_list = []
-for num in range(10):
-    selected_color = colors[num]
-    rgb = selected_color.rgb
-    red = rgb.r
-    green = rgb.g
-    blue = rgb.b
-    rgb_color = (red, green, blue)
-    color_list.append(rgb_color)
-print(color_list)
-print(colors)
+# colors = colorgram.extract('image.jpg',10)
+# color_list = []
+# for num in range(10):
+#     selected_color = colors[num]
+#     rgb = selected_color.rgb
+#     red = rgb.r
+#     green = rgb.g
+#     blue = rgb.b
+#     rgb_color = (red, green, blue)
+#     color_list.append(rgb_color)
+# print(color_list)
 # print(colors)
-print(len(colors))
+# # print(colors)
+# print(len(colors))
+
+colors_list = [ (234, 225, 83), (195, 8, 69), (231, 54, 132), (194, 164, 15), (112, 178, 214), (199, 77, 15)] 
